fd9c30820468781c401e17b30eb5f59367d0e78d
Flame-Y <1248993272@qq.com>
2023-08-09 19:56:59 +0800

    :sparkles: feat: connect to database (mysql)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root', //数据库用户名配置
+            password: '114514', //数据库密码配置
+            database: 'apicat', //数据库名字配置
+            synchronize: true,
+            logging: true,
+            entities: [User],
+            poolSize: 10,
+            connectorPackage: 'mysql2',
+            extra: {
+                authPlugin: 'sha256_password'
+            }
+        }),
+        UserModule
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
